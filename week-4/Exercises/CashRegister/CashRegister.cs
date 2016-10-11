e25dfd4e5dda7c08494d86df7dfe4e93ec383375
Brian McGowan <mcgowan.b@gmail.com>
2016-10-12 00:01:53 +0100

    adding question 4  work

---
+++ week-4/Exercises/CashRegister/CashRegister.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCashRegister
+{
+    class CashRegister
+    {
+        private double total;
+        private int numberOfItems;
+        public double Total { get { return total; } }
+        public int NumberOfItems { get { return numberOfItems; } }
+        public String Name { get; set; }
+        public static double totalCash;
+        public static int totalItems;
+
+        public CashRegister(String name)
+        {
+            Name = name;
+        }
+
+        public void addItem(double price)
+        {
+            total += price;
+            totalCash += price;
+            numberOfItems++;
+            totalItems++;
+            Console.WriteLine("Adding item worth {0} to {1}",
+                price,
+                Name
+                );
+        }
+
+
+
+
+
+    }
+}
---
+++ week-4/Exercises/CashRegister/TestCashRegister.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCashRegister
+{
+    class TestCashRegister
+    {
+        static void Main(string[] args)
+        {
+            CashRegister cr1 = new CashRegister("Cash Register 1");
+            cr1.addItem(2.70);
+            cr1.addItem(3.45);
+            cr1.addItem(5.97);
+
+            CashRegister cr2 = new CashRegister("Cash Regster 2");
+            cr2.addItem(12.52);
+            cr2.addItem(1.43);
+            cr2.addItem(15.57);
+            cr2.addItem(5.15);
+
+            Console.WriteLine("\n{0} Total: {1}\nCash Register CR1 Total Number of Items: {2}", cr1.Name, cr1.Total, cr1.NumberOfItems);
+            Console.WriteLine("{0} Total: {1}\nCash Register CR2 Total Number of Items: {2}", cr2.Name, cr2.Total, cr2.NumberOfItems);
+
+            Console.WriteLine("\nTotal money from all cash registers: {0}", CashRegister.totalCash);
+            Console.WriteLine("Total items from all cash registers: {0}", CashRegister.totalItems);
+        }
+    }
+}
