e68bc555d42f1c8281c45defbed3d87f99299a49
Brian McGowan <mcgowan.b@gmail.com>
2016-10-27 14:48:55 +0100

    simple file reading / writing example

---
+++ week-7/Revision/CA Revision/Friend.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_Revision
+{
+    class Friend : IComparable
+    {
+        private const String lineBreak = "=============================================";
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+
+        public Friend(String name, String phone, int day, int month, int year)
+        {
+            Name = name;
+            PhoneNumber = phone;
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+        public override string ToString()
+        {
+            return String.Format("Name: {0}\nPhone: {1}\nDOB: {2}/{3}/{4}\n{5}", Name, PhoneNumber, Day, Month, Year, lineBreak);
+
+        }
+
+        internal string FileFormat()
+        {
+            return String.Format("{0},{1},{2}/{3}/{4}", Name, PhoneNumber, Day, Month, Year);
+        }
+
+        public int CompareTo(object obj)
+        {
+            Friend tmp = (Friend)obj;
+            return this.Name.CompareTo(tmp.Name);
+        }
+    }
+}
---
+++ week-7/Revision/CA Revision/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_Revision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] linesInFile = File.ReadAllLines("data.txt");
+            Friend[] friends = new Friend[linesInFile.Length];
+            string[] newThings = new string[linesInFile.Length];
+
+            for (int i = 0; i < linesInFile.Length; i++)
+            {
+                string[] lData = linesInFile[i].Split(',');
+                string n = lData[0];
+                string p = lData[1];
+                int d = Convert.ToInt32(lData[2]);
+                int m = Convert.ToInt32(lData[3]);
+                int y = Convert.ToInt32(lData[4]);
+
+                Friend f = new Friend(n, p, d, m, y);
+                friends[i] = f;
+            }
+
+            foreach (Friend f in friends)
+            {
+                Console.WriteLine(f);
+            }
+            Console.WriteLine();
+            Array.Sort(friends);
+
+            for (int i = 0; i < linesInFile.Length; i++)
+            {
+                Console.WriteLine(friends[i]);
+                newThings[i] = friends[i].FileFormat();
+            }
+
+            File.WriteAllLines("notFunny.txt", newThings);
+
+        }
+    }
+}
