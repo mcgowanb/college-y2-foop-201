fa92f175473a5fd65e284eee046cad80949dc3e6
Brian McGowan <mcgowan.b@gmail.com>
2016-11-22 10:54:07 +0000

    classwork week 9

---
+++ week-9/ShoppingCart/ShoppingCart/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingCart
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Product> products;
+        private ObservableCollection<Product> cart;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            products = new ObservableCollection<Product>();
+            cart = new ObservableCollection<Product>();
+
+            Product p1 = new Product("1324", "Book", 12.53m);
+            products.Add(p1);
+            Product p2 = new Product("1325", "Bike", 780m);
+            products.Add(p2);
+            Product p3 = new Product("1326", "Boat", 900m);
+            products.Add(p3);
+
+            lbxProducts.ItemsSource = products;
+            lbxCart.ItemsSource = cart;
+
+        }
+
+        private void btnAddTax_Click(object sender, RoutedEventArgs e)
+        {
+            lbxProducts.ItemsSource = null;
+            foreach (var item in products)
+            {
+                item.AddTax();
+            }
+            lbxProducts.ItemsSource = products;
+        }
+
+        private void btnAddToCart_Click(object sender, RoutedEventArgs e)
+        {
+            Product selectedProduct = lbxProducts.SelectedItem as Product;
+
+            if (selectedProduct != null)
+            {
+                cart.Add(selectedProduct);
+                products.Remove(selectedProduct);
+            }
+        }
+
+        private void btnRemoveFromCart_Click(object sender, RoutedEventArgs e)
+        {
+            Product selectedProduct = lbxCart.SelectedItem as Product;
+            if (selectedProduct != null)
+            {
+                cart.Remove(selectedProduct);
+                products.Add(selectedProduct);
+            }
+        }
+    }
+}
---
+++ week-9/ShoppingCart/ShoppingCart/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public decimal Price { get; set; }
+        public const decimal TAX_RATE = .23m;
+
+        public Product(string id, string name, decimal price)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\t{1}\t{2}",
+                ID,
+                Name,
+                Price.ToString("C")
+            );
+        }
+
+        public void AddTax()
+        {
+            Price += (Price * TAX_RATE);
+        }
+    }
+}
