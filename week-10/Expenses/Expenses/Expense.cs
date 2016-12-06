3df9ff00de429b8eb292893d472e7ebb30cb13a0
Brian McGowan <mcgowan.b@gmail.com>
2016-12-06 10:31:51 +0000

    week 10 classwork

---
+++ week-10/Expenses/Expenses/Expense.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expenses
+{
+    class Expense
+    {
+
+        public String Type { get; set; }
+        public String Date { get; set; }
+        public decimal Cost { get; set; }
+
+        private String[] types = { "Travel", "Entertainment", "Office" };
+
+        private static Random randomFactory = new Random();
+
+        public Expense()
+        {
+            Type = getRandomType();
+            Date = getRandomDate();
+            Cost = getRandomCost();
+        }
+
+        private decimal getRandomCost()
+        {
+            decimal cost = (randomFactory.Next(10001) / 100);
+            return cost;
+        }
+
+        private String getRandomType()
+        {
+            int rnd = randomFactory.Next(0, 2);
+            return types[rnd];
+        }
+
+        private String getRandomDate()
+        {
+            DateTime d = DateTime.Today.AddDays(randomFactory.Next(0, 31));
+            return d.ToShortDateString();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1:C} on {2}", Type, Cost, Date);
+        }
+    }
+}
---
+++ week-10/Expenses/Expenses/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expenses
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Expense> expenses;
+        decimal total;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddExpense_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            expenses = new List<Expense>();
+            total = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                Expense exp = new Expense();
+                expenses.Add(exp);
+                total += exp.Cost;
+            }
+            lbxExpenses.ItemsSource = expenses;
+            lblTotal.Content = total.ToString("C", CultureInfo.CurrentCulture);
+        }
+    }
+}
